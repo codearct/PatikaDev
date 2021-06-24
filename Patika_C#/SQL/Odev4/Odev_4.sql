90a541780dc29ac55f1f9fe694d539eead208ba8
codearct <codearct@gmail.com>
2021-06-25 00:07:23 +0300

    SQL dersi Ödev 4 yapıldı.

---
+++ Patika_C#/SQL/Odev4/Odev_4.sql
@@ -0,0 +1,12 @@
+--SELECT DISTINCT replacement_cost FROM film;
+
+--SELECT COUNT (DISTINCT replacement_cost) FROM film;
+
+--SELECT COUNT(title) FROM film
+--WHERE title LIKE 'T%' AND rating = 'G';
+
+--SELECT COUNT(country) FROM country
+--WHERE country LIKE '_____';
+
+--SELECT COUNT(city) FROM city
+--WHERE city ILIKE '%r';
