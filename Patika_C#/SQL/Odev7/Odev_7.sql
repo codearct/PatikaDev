61ef0c62ec77a38de8792f83fa138db04485df38
codearct <codearct@gmail.com>
2021-06-25 22:30:10 +0300

    SQL dersi Ödev 7 yapıldı.

---
+++ Patika_C#/SQL/Odev7/Odev_7.sql
@@ -0,0 +1,14 @@
+--SELECT rating FROM film
+--GROUP BY rating;
+
+--SELECT replacement_cost,COUNT(*) FROM film
+--GROUP BY replacement_cost
+--HAVING COUNT(*)>50;
+
+--SELECT store_id,COUNT(*) FROM customer
+--GROUP BY store_id;
+
+--SELECT country_id,COUNT(city) FROM city
+---GROUP BY country_id
+--ORDER BY COUNT(city) DESC
+--LIMIT 1;
