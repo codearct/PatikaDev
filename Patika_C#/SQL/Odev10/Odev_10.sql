1d6ea1b94290bc9145fcf417483adc5ae7bc6847
codearct <codearct@gmail.com>
2021-06-27 16:20:09 +0300

    SQL dersi Ödev 10 yapıldı.

---
+++ Patika_C#/SQL/Odev10/Odev_10.sql
@@ -0,0 +1,9 @@
+-- SELECT city,country FROM city
+-- LEFT JOIN country ON city.country_id=country.country_id;
+
+-- SELECT payment_id,first_name,last_name FROM payment
+-- RIGHT JOIN customer ON payment.customer_id=customer.customer_id;
+
+-- SELECT rental.rental_id,customer.first_name,customer.last_name FROM rental
+-- FULL JOIN customer ON customer.customer_id=rental.customer_id;
+
