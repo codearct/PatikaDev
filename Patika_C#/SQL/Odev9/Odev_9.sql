4f0ef38fbbe2445b285d08beaa7097fd873ab4d6
codearct <codearct@gmail.com>
2021-06-27 15:07:42 +0300

    SQL dersi Ödev 9 yapıldı.

---
+++ Patika_C#/SQL/Odev9/Odev_9.sql
@@ -0,0 +1,6 @@
+-- SELECT city,country FROM city
+-- INNER JOIN country ON city.country_id=country.country_id;
+
+-- SELECT payment_id,first_name,last_name FROM payment
+-- INNER JOIN customer ON payment.customer_id=customer.customer_id;
+
