b16db5b48815e22ec24bb9d98f322e3d483c065b
codearct <codearct@gmail.com>
2021-06-29 00:41:26 +0300

    Csharp dersi operatörler konusu işlendi.

---
+++ Patika_C#/Csharp101/Operatorler/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------Atama ve İşlemli Atama--------");
+            // Atama ve İşlemli Atama
+            int x = 3;
+            int y = 3;
+
+            y = y + 2;
+            Console.WriteLine(y);
+
+            y += 2;
+            Console.WriteLine(y);
+
+            y /= 1;
+            Console.WriteLine(y);
+
+            x *= 2;
+            Console.WriteLine(x);
+
+            Console.WriteLine("-------Mantıksal Operatörler--------");
+            //Mantıksal Operatörler
+            // ||,&&,!
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+                Console.WriteLine("Perfect!");//Passed because true false
+
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Great!");
+
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine!");
+
+            Console.WriteLine("-------İlişkisel Operatörler--------");
+            // İlişkisel Operatörler
+            // <,>,>=,<=,==,!=
+
+            int a = 3;
+            int b = 4;
+
+            bool sonuc = a < b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine("-------Aritmatik Operatörler--------");
+            // Aritmetik Operatörler
+            // +,-,*,/,%
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1 / sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 - sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+
+            int sonuc2 = 20 % 3;
+            Console.WriteLine(sonuc2);
+
+
+
+
+
+
+        }
+    }
+}
