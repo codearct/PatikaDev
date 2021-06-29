16199a62f378dd8a51288738cc1831d02ab4b7ba
codearct <codearct@gmail.com>
2021-06-30 01:41:25 +0300

    Csharp101 dersi For döngüsü işlendi.

---
+++ Patika_C#/Csharp101/Donguler_For/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Donguler_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ekrandan girilen sayıya kadar olan tek sayıları ekrana yazdır.
+            Console.Write("Lütfen bir sayı giriniz :");
+            int sayac = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine(i);
+            }
+
+            //1 ile 1000 arasındaki tek ve çift sayıları toplayıp ekrana yazdıralım.
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                    tekToplam += i;
+                else
+                    ciftToplam += i;
+            }
+            Console.WriteLine("Tek Toplam :" + tekToplam);
+            Console.WriteLine("Çift Toplam :" + ciftToplam);
+
+            //break,continue
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
