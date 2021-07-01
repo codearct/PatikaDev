1c42dfd5a851f0617fd97244abc90906dddc51f6
codearct <codearct@gmail.com>
2021-07-01 10:28:59 +0300

    Csharp101 dersi while ve foreach döngüleri işlendi

---
+++ Patika_C#/Csharp101/while_foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 den başlayarak console dan girilen sayıya kadar(sayı dahil) ortalama hesaplayıp console a yazdıran program
+            Console.Write("Lütfen bir sayı giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            // 'a'dan 'z'ye kadar tüm harfleri console a yazdır
+
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (string araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+
+    }
+}
