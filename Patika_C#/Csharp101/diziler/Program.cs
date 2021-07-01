f696a1a9e3800aee852598ac95093de316bbf001
codearct <codearct@gmail.com>
2021-07-01 23:43:08 +0300

    Csharp101 dersi diziler konusu işlendi.

---
+++ Patika_C#/Csharp101/diziler/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımla
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = { "kedi", "köpek", "kuş", "maymun" };
+
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere değer atama ve erişim
+            renkler[0] = "mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+            // Döngüler dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını hesaplayan program
+
+            Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}.sayıyı giriniz: ", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+
+            foreach (var sayi in sayiDizisi)
+                toplam += sayi;
+
+            Console.WriteLine("ortalama: " + toplam / diziUzunlugu);
+
+
+        }
+    }
+}
