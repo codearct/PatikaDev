9c8bfba5e8eb820d61075e95c96e08dda9ccc7e7
codearct <codearct@gmail.com>
2021-07-06 15:30:11 +0300

    Csharp101 dersi arrayList konusu işlendi.

---
+++ Patika_C#/Csharp101/arrayList/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Collections namespace
+            ArrayList liste = new ArrayList();
+            liste.Add("Ayşe");
+            liste.Add(2);
+            liste.Add(true);
+            liste.Add('A');
+
+            //içerisindeki verilere erişim
+            Console.WriteLine(liste[1]);
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //AddRange
+            Console.WriteLine("-----Add Range-----");
+            string[] renkler = { "kırmızı", "sarı", "yeşil" };
+            List<int> sayilar = new List<int>() { 1, 8, 3, 7, 9, 92, 5 };
+            liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort
+            Console.WriteLine("-----Sort-----");
+            //liste.Sort() hata verir çünkü çeşitli veri tipleri içeride
+
+            //BinarySearch
+            //liste.BinarySearch(9) hata verir.Önce sıralayıp sonra binary search yapmalısın.
+
+            //Reverse
+            liste.Reverse();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //liste.Clear() listeyi temizler.
+
+
+
+        }
+    }
+}
