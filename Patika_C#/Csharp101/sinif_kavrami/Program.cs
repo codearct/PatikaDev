c97b3990415a7c9d408deb89543a64b3144ae8e5
codearct <codearct@gmail.com>
2021-07-08 15:10:41 +0300

    Csharp101 dersi sınıf kavramı konusu işlendi.

---
+++ Patika_C#/Csharp101/sinif_kavrami/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Söz Dizimi
+            //class SinifAdi
+            //{
+            //      [Erişim Belirleyici] [Veri Tipi] OzellikAdi;
+            //      [Erişim Belirleyici] [Geri Dönüş Tipi] MetodAdi(Parametre Listesi)
+            //      {
+            //              Metot Komutları
+            //      }    
+            //}
+
+            //Erişim Belirleyiciler
+            // * Public
+            // * Private
+            // * Internal
+            // * Protected
+
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ayşe";
+            calisan1.Soyad = "Kara";
+            calisan1.No = 2345623;
+            calisan1.Departman = "İnsan Kaynakları";
+
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine("------------------");
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Mehmet Akif";
+            calisan2.Soyad = "Yıldız";
+            calisan2.No = 250182;
+            calisan2.Departman = "Mimarlık";
+
+            calisan2.CalisanBilgileri();
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı: {0}", Ad);
+            Console.WriteLine("Çalışanın Soyadı: {0}", Soyad);
+            Console.WriteLine("Çalışanın Numarası: {0}", No);
+            Console.WriteLine("Çalışanın Departmanı: {0}", Departman);
+        }
+    }
+}
+
