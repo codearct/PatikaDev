7cb90b0c627252896edc9a75adaac5de690b88cc
codearct <codearct@gmail.com>
2021-10-22 13:19:50 +0300

    Contact-App yapıldı.

---
+++ FrontEnd/React/Components/Contacts-App/src/components/Contacts/Form/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+
+const initialFormValues = { fullname: "", phone_number: "" }
+
+function Form({ addContact, contacts }) {
+
+    const [form, setForm] = useState(initialFormValues);
+
+    useEffect(() => {
+        setForm(initialFormValues)
+    }, [contacts]);
+
+    const onChangeInput = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (form.fullname === "" || form.phone_number === "") {
+            return false
+        }
+
+        addContact([...contacts, form]);
+
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div className="formInput">
+                <input
+                    name="fullname"
+                    placeholder="Full Name"
+                    onChange={onChangeInput}
+                    value={form.fullname}
+                />
+            </div>
+            <div>
+                <input
+                    name="phone_number"
+                    placeholder="Phone Number"
+                    onChange={onChangeInput}
+                    value={form.phone_number}
+                />
+            </div>
+            <div className="btn">
+                <button>Add</button>
+            </div>
+
+        </form>
+    )
+}
+
+export default Form
---
+++ FrontEnd/React/Components/Contacts-App/src/components/Contacts/List/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function List({ contacts }) {
+    const [filterText, setFilterText] = useState("")
+
+    const filtered = contacts.filter((item) => {
+        return Object.keys(item).some((key) =>
+            item[key].toString().toLowerCase()
+                .includes(filterText.toLowerCase())
+        );
+    })
+
+
+    return (
+        <div>
+            <input
+                placeholder="Filter Contacts"
+                value={filterText}
+                onChange={(e) => setFilterText(e.target.value)}
+            />
+
+            <ul className="list">
+                {
+                    filtered.map((contact, i) =>
+                        <li key={i}>
+                            <span>
+                                {contact.fullname}
+                            </span>
+                            <span>
+                                {contact.phone_number}
+                            </span>
+                        </li>
+                    )
+                }
+            </ul>
+
+            <p>Total Contacts : {filtered.length}</p>
+        </div>
+    )
+}
+
+export default List
